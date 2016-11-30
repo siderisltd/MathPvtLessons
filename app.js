48feb69278873d7146b3ff36b85ebb1e9150b58a
Aleksis Sideris <Aleksis Sideris>
2016-11-30 16:58:54 +0200

    Innitial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var request = require('request');
+var engine = require('ejs-mate');
+
+var app = express();
+
+var dev = true;
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs')
+
+app.use("/res", express.static(__dirname + '/res'));
+app.use("/node_modules", express.static(__dirname + '/node_modules'));
+
+app.use("/dist", express.static(__dirname + '/dist'));
+app.use("views", express.static(__dirname + '/views'));
+
+
+app.get('/', function(req, res) {
+    res.render('pages/index', { dev: dev })
+});
+
+app.get('*', function(req, res) {
+    res.send('what???', 404);
+});
+
+setInterval(function() {
+    request('https://calingappreqs.herokuapp.com/', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Show the HTML for the Google homepage.
+        }
+    })
+}, 20 * 1000 * 60); // 20 mins
+
+
+app.listen(process.env.PORT || 3000);
